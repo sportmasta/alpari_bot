4c5418cc99a076feb8737676410aacd2bf722bee
sportmasta <atankovv@gmail.com>
2025-08-09 01:06:36 +0800

    delete files and restore space line

--- bot_gpt1.py
+++ bot_gpt1.py
@@ -11,6 +11,7 @@ from dotenv import load_dotenv
 
 # Загрузка переменных окружения
 load_dotenv()
+
 # Настройка логирования
 logging.basicConfig(filename='TGBot.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
